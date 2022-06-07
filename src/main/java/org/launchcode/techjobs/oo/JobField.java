dd8c6361164bdd88efdf2dd516dbf6eb5c3d2d58
Delphine  Dara <delphine.dara@gmail.com>
2022-06-06 20:04:16 -0500

    made corrections in Jobfield class, also used super class in PositionType class

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -29,9 +29,9 @@ public abstract class JobField {
     @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
         if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
-        Employer employer = (Employer) o;
-        return getId() == employer.getId();
+        if (!(o instanceof JobField)) return false;
+        JobField jobfield = (JobField) o;
+        return getId() == jobfield.getId();
     }
 
     @Override
